17e59c7f856083340c39c558e5a8388a64ef5e4c
ViacheslavKras <emailkras@gmail.com>
2021-01-07 15:55:22 +1000

    Add Swiper

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 import './app.css';
 
 const App = () => {
-    return <h1>App</h1>
+    return (
+        <Swiper>
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
+    )
 };
 
 export default App;
