4e1fe39851e08a45d47eb79c0ff4cfcdc0b6c02b
ViacheslavKras <emailkras@gmail.com>
2021-01-08 22:44:02 +1000

    Apply style to slides

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ SwiperCore.use([Navigation, Pagination, Autoplay, Keyboard]);
 
 const App = () => {
     const slidesData = [
-        { name: 'HTML', description: 'Something about HTML', id: 1 },
-        { name: 'CSS', description: 'Something about CSS', id: 2 },
-        { name: 'JavaScript', description: 'Something about JavaScript', id: 3 },
-        { name: 'React', description: 'Something about React', id: 4 }
+        { name: 'HTML', description: 'Something about HTML', className: 'html-slide', id: 1 },
+        { name: 'CSS', description: 'Something about CSS', className: 'css-slide', id: 2 },
+        { name: 'JavaScript', description: 'Something about JavaScript', className: 'javascript-slide', id: 3 },
+        { name: 'React', description: 'Something about React', className: 'react-slide', id: 4 }
     ];
     const slides = slidesData.map((item) => {
         return (
             <SwiperSlide key={item.id}>
-                <div>
+                <div className={item.className}>
                     <h1>{item.name}</h1>
                     <p>{item.description}</p>
                 </div>
